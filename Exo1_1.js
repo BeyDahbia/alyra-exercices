7c168490b7ba1ac8bcb2a87b52443052076486b9
Bey <beydahbia@gmail.com>
2019-01-23 18:20:33 +0100

    Exo1_1

---
+++ Exo1_1.js
@@ -0,0 +1,5 @@
+function calculerDificulte(cible){
+	var max =2.7*Math.pow(106,67)
+	console.log(max/cible);
+}
+calculerDifficulte(1147152896345386682952518188670047452875537662186691235300769792000);
